3f33f05735ea52b8b1b0fe58f5c1e8b6d6f61ed2
maximaarminger <maxima.arminger@htl-salzburg.ac.at>
2019-12-04 11:35:12 +0100

    added Sample Score

---
+++ Assets/My Games/Scripts/ScoreExample.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreExample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetScore(6, "Maxim Mustermann");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetScore(8, "Maxima Arminger");
+    }
+
+    public void SetScore(int score, string name)
+    {
+        int scoreValue = score;
+        string nameValue = name;
+        Debug.Log("score = " + scoreValue + "name = " +nameValue);
+    }
+}
